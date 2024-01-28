c43bd61af1b44b5a00b8d1f92105cccd1b096579
kingofnorth1 <3441423164@qq.com>
2024-01-28 18:31:09 +0800

    定时器定时中断切换为外部时钟

--- 6.1 定时器定时中断/System/Timer.c
+++ 6.1 定时器定时中断/System/Timer.c
@@ -6,18 +6,27 @@
 void Timer_Init(void)
 {
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     
-    TIM_InternalClockConfig(TIM2);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    
+//    TIM_InternalClockConfig(TIM2);
+    TIM_ETRClockMode2Config(TIM2, TIM_ExtTRGPSC_OFF, TIM_ExtTRGPolarity_Inverted, 0xFF);
     
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
     TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
     TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-    TIM_TimeBaseInitStructure.TIM_Period = 10000-1;
-    TIM_TimeBaseInitStructure.TIM_Prescaler = 7200-1;
+    TIM_TimeBaseInitStructure.TIM_Period = 10 -1;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 1 -1;
     TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
     TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStructure);
     
-    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+    TIM_ClearFlag(TIM2, TIM_FLAG_Update);
+        (TIM2, TIM_IT_Update, ENABLE);
     
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
     
@@ -29,6 +38,11 @@ void Timer_Init(void)
     NVIC_Init(&NVIC_InitStructure);
     
     TIM_Cmd(TIM2, ENABLE);
+}
+
+uint16_t Timer_GetCounter(void)
+{
+    return TIM_GetCounter(TIM2);
 }
 
 //void TIM2_IRQHandler(void)
