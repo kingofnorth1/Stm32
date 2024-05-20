8dc31ab30c4205b9d565f8ed62e83026e4fcd409
kingofnorth1 <3441423164@qq.com>
2024-05-21 03:53:07 +0800

    添加软件I2C的读写

---
+++ 10-1 软件I2C读写/User/main.c
@@ -0,0 +1,22 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "time.h"  
+#include "MyI2C.h"
+
+int main(void)
+{
+	OLED_Init();
+    
+    MyI2C_Init();
+    MyI2C_Start();
+    MyI2C_SendByte(0xD0);
+    uint8_t Ack = MyI2C_ReceiveAck();
+	MyI2C_Stop();
+    
+    OLED_ShowNum(1, 1, Ack, 3);
+    while(1)
+    {
+        
+    }
+}
